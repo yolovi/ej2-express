c15ad1ef8f0c352cf0d559b70c84c7e3cdc03222
yolovi <yolandalopevi@gmail.com>
2023-06-28 15:49:36 +0200

    first commit

---
+++ ej2.js
@@ -0,0 +1,14 @@
+// Ejercicio 2
+// Crear un archivo con el nombre ej2.js
+// Levantar un servidor de Express
+// Manejar las siguientes rutas:
+// Ruta: Raíz del sitio (‘/’) ,Metodo: get, Acción: Mostrar un mensaje de bienvenida
+// Ruta: Productos, Método: get, Acción: Mostrar un mensaje que diga: listado de productos
+// Ruta: Productos, Método: post, Acción: Mostrar un mensaje que diga: crear un producto
+// Ruta: Productos, Método: put, Acción: Mostrar un mensaje que diga: actualizar un producto
+// Ruta: Productos, Método: delete, Acción: Mostrar un mensaje que diga: borrar un producto
+// Ruta: Usuarios, Metodo: get, Acción: Mostrar un mensaje que diga: listado de usuarios
+// Ruta: Usuarios, Método: post, Acción: Mostrar un mensaje que diga: crear un usuario
+// Ruta: Usuarios, Metodo: put, Acción: Mostrar un mensaje que diga: actualizar un usuario
+// Ruta: Usuarios, Metodo: delete, Acción: Mostrar un mensaje que diga: borrar un usuario
+// Utilizar Postman para probar todos los llamados
